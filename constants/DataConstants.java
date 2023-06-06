9d085c672ec3608def789edc7e800153624b6bb3
Taran gangil <40990378+taran12345@users.noreply.github.com>
2023-06-07 00:28:54 +0530

    Add files via upload

---
+++ constants/DataConstants.java
@@ -0,0 +1,86 @@
+// All Rights Reserved, Copyright © Paysafe Holdings UK Limited 2017. For more information see LICENSE
+
+package com.paysafe.upf.user.provisioning.web.rest.constants;
+
+public final class DataConstants {
+
+  public static final String USER_NOT_FOUND = "No user found";
+  public static final String TOKEN_NOT_FOUND_LOGIN_NAME = "No token found for username %s";
+  public static final String TOKEN_EXPIRED = "invalid token - token expired";
+  public static final String PORTAL = "PORTAL";
+  public static final String SKRILL = "SKRILL";
+  public static final String NETELLER = "NETELLER";
+  public static final String WALLETS = "WALLETS";
+  public static final String PMLE = "PMLE";
+  public static final String MLE = "MLE";
+  public static final String REGULAR = "REGULAR";
+  public static final String ADMIN = "ADMIN";
+  public static final String OWNER_ID = "ownerId";
+  public static final String OWNER_TYPE = "ownerType";
+  public static final String BP_ADMIN = "BP_ADMIN";
+  public static final String BP_EU_ADMIN = "BP_EU_ADMIN";
+  public static final String BP_DEVELOPER = "BP_DEVELOPER";
+  public static final String BP_BUSINESS = "BP_BUSINESS";
+  public static final String BP_OPERATION = "BP_OPERATION";
+  public static final String BP_ISV_ADMIN = "BP_ISV_ADMIN";
+  public static final String IS_MIGRATED = "isMigrated";
+  public static final String BUSINESS_UNIT = "businessUnit";
+  public static final String PAYMENT_ACCOUNT = "PAYMENT_ACCOUNT";
+  public static final String PASS_RECOVERY_TOKEN_TYPE = "PASSWORD_RECOVERY";
+  public static final String CUSTOMER_NAME = "custName";
+  public static final String LOGIN_NAME = "loginName";
+  public static final String PARTNER = "PARTNER";
+  public static final String ACCOUNT_GROUP = "ACCOUNT_GROUP";
+  public static final String CUSTOMER_ID = "customerId";
+  public static final String REGISTRATION_MAIL = "REGISTRATION_MAIL";
+  public static final String PERMISSIONS_UPDATED_MAIL = "PERMISSIONS_UPDATED_MAIL";
+  public static final String ACTIVATION_EMAIL_EVENTID = "userActivationEmailEventId";
+  public static final String PERMISSIONS_UPDATED_EMAIL_EVENTID = "permissionsUpdatedEmailEventId";
+  public static final String ACTIVATE_URI = "/activate";
+  public static final String LOGIN_URI = "/v1/login";
+  public static final String USER = "USER";
+  public static final String FMA = "FMA";
+  public static final String SKRILL_BUSINESS_CONTACT_EMAIL = "businessContactEmailEventId";
+  public static final String BUSINESS = "BUSINESS";
+  public static final String NETELLER_LOCK_LEVEL_22 = "22";
+  public static final String SKRILL_LOCK_LEVEL_17 = "17";
+  public static final String SKRILL_LOCK_LEVEL_2 = "2";
+  public static final String MERCHANT_SETTINGS = "merchant-settings";
+  public static final String PROFILE_SETTINGS = "profile-settings";
+  public static final String BUSINESS_PROFILE = "business-profile";
+  public static final String PROFILE = "profile";
+  public static final String MONEY_FLOWS = "money-flows";
+  public static final String DEFAULT_ACCOUNT = "default-account";
+  public static final String EMAILS = "emails";
+  public static final String EWALLET_ACCOUNTS = "ewallet-accounts";
+  public static final String PARTNER_PORTAL = "PARTNER_PORTAL";
+  public static final String APPLICATION = "application";
+  public static final String PAYSAFE_BRAND = "Paysafe";
+  public static final String EU_ACQUIRING_EEA = "EU_ACQUIRING_EEA";
+  public static final String EU_ACQUIRING_NON_EEA = "EU_ACQUIRING_NON_EEA";
+  public static final String BP_BINANCE_ADMIN = "BP_BINANCE_ADMIN";
+  public static final String BINANCE = "BINANCE";
+  public static final String US_IGAMING_ROLE_STRING = "_US_";
+  public static final String EU_ROLE_STRING = "_EU_";
+  public static final String ECOMM_ROLE_STRING = "_ECOMM_";
+  public static final String UNDEFINED = "undefined";
+  public static final String NULL = "null";
+  public static final String DIVISION = "division";
+  public static final String NBX_PORTAL = "NBX_PORTAL";
+  public static final String USER_ASSIGNED_APPLICATIONS = "user_assigned_applications";
+  public static final String LOGIN_APP = "LOGIN-APP";
+  public static final String MFA_GROUP = "MFA_GROUP";
+  public static final String PROFILE_SETTINGS_MFA_ENABLED_EVENTID = "profileSettingsMfaEnabledEventId";
+  public static final String PROFILE_SETTINGS_MFA_DISABLED_EVENTID = "profileSettingsMfaDisabledEventId";
+  public static final String ADMIN_MFA_ENABLED_INAPP_EVENTID = "adminMfaEnabledInAppEventId";
+  public static final String SUBUSER_MFA_ENABLED_EMAIL_EVENTID = "subUserMfaEnabledEmailEventId";
+  public static final String ADMIN_MFA_DISABLED_INAPP_EVENTID = "adminMfaDisabledInAppEventId";
+  public static final String SUBUSER_MFA_DISABLED_EMAIL_EVENTID = "subUserMfaDisabledEmailEventId";
+  public static final String RESET_MFA_EMAIL_EVENTID = "resetMfaEmailEventId";
+  public static final String ADMIN_RESET_MFA_INAPP_EVENTID = "adminResetMfaInAppEventId";
+  public static final String ADMIN_BULK_UPDATE_SUCCESS_MFA_INAPP_EVENTID = "adminBulkUpdateSuccessMfaInAppEventId";
+  public static final String ADMIN_BULK_FAILURE_SUCCESS_MFA_INAPP_EVENTID = "adminBulkFailureSuccessMfaInAppEventId";
+
+  private DataConstants() {
+  }
+}
