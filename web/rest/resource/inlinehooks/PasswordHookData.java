e3a8d85be3ba6be88cf091ecf67e36bc72457d2c
Taran gangil <40990378+taran12345@users.noreply.github.com>
2023-06-07 00:35:39 +0530

    Add files via upload

---
+++ web/rest/resource/inlinehooks/PasswordHookData.java
@@ -0,0 +1,23 @@
+// All Rights Reserved, Copyright © Paysafe Holdings UK Limited 2017. For more information see LICENSE
+
+package com.paysafe.upf.user.provisioning.web.rest.resource.inlinehooks;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PasswordHookData {
+
+  PasswordHookContext context;
+  PasswordHookAction action;
+}
---
+++ web/rest/resource/inlinehooks/PasswordImportResponseResource.java
@@ -0,0 +1,24 @@
+// All Rights Reserved, Copyright © Paysafe Holdings UK Limited 2017. For more information see LICENSE
+
+package com.paysafe.upf.user.provisioning.web.rest.resource.inlinehooks;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class PasswordImportResponseResource {
+
+  List<PasswordHookResponseCommand> commands;
+}
